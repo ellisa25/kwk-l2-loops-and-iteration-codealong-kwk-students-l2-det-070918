cc71b3ed47707cf3f153d71e97e9ecdbe1ae5356
ellisa25 <ewhill0306@gmail.com>
2018-07-16 13:45:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ function printBadges (names) {
 }
   return names;
   }
-  
+   function tailsNeverFails () {
+     let tails = 0
+     
+     while (Math.random()>=0.5) {
+       tails++
+     }
+     return 'You got ${tails} tails in a row';
+   }
 
