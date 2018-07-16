99424929748cfd08e50ab6faa299c6f6c641f860
ellisa25 <ewhill0306@gmail.com>
2018-07-16 13:36:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 function printBadges (names) {
   for (let i = 0; i < names; i++)
   
+  console.log('Welcome ${names[i]}! You are employee #${i + 1}.');
 }
+
 const gifts = ["teddy bear", 'drone', 'doll'];
 function setAside (gift) {
   console.log('Wrapped ${gift} and added a card!');
