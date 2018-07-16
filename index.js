3891eba2754fab65979701dcc7e89226ca9662cb
ellisa25 <ewhill0306@gmail.com>
2018-07-16 13:26:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Code your solutions in this file
 const gifts = ["teddy bear", 'drone', 'doll'];
 function setAside (gift) {
-  console.log('Wrapped 
-${gift} and added a card!');
+  console.log('Wrapped ${gift} and added a card!');
 }
