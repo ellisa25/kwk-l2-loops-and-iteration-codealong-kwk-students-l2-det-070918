398a3bbc330bcef27f193d55fee1e92714e7891a
ellisa25 <ewhill0306@gmail.com>
2018-07-16 13:29:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solutions in this file
+function printBadges (names) {
+  
+}
 const gifts = ["teddy bear", 'drone', 'doll'];
 function setAside (gift) {
   console.log('Wrapped ${gift} and added a card!');
